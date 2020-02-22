4dd2945678ae056fd5ad87d9619b8bb7165d71a0
Bertrand Warot <bw.develop@yahoo.com>
2020-02-22 14:01:29 +0100

    Iconic boot sound

--- post-install.sh
+++ post-install.sh
@@ -179,7 +179,6 @@ killall Dock
 # Iconic Mac boot sound
 sudo nvram StartupMute=%00
 
-
 ## Cherry on the top
 # Oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
